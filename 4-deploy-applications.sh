92002847efed9c23f33f92594b2c484ab3077078
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-07-22 21:36:45 +0200

    fix: add argo apps

--- 4-deploy-applications.sh
+++ 4-deploy-applications.sh
@@ -18,20 +18,3 @@ kubectl apply -k k8s/external-secrets-operator
 
 kubectl apply -k k8s/argocd # first attempt will fail due to missing crds
 kubectl apply -k k8s/argocd
-
-# kubectl apply -k k8s/tekton # first attempt will fail due to missing crds
-# kubectl apply -k k8s/tekton
-
-# kubectl apply -k k8s/external-dns # first attempt will fail due to missing crds
-# kubectl apply -k k8s/external-dns
-
-# kubectl apply -k k8s/traefik # first attempt will fail due to missing crds
-# kubectl apply -k k8s/traefik
-
-# kubectl apply -k k8s/certmanager # first attempt will fail due to missing crds
-# kubectl wait deployment -n cert-manager cert-manager-webhook --for condition=Available=True --timeout=120s
-# kubectl apply -k k8s/certmanager
-
-# kubectl apply -k k8s/tekline
-
-# kubectl apply -k k8s/renovate
