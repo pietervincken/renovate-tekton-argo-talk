fe30e70f29384adf144d72383035c6d90be516a5
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-09-30 11:52:07 +0200

    fix upload script

--- 4-upload-app-image.sh
+++ 4-upload-app-image.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Make sure to update to latest tag! Using 1.4.4 now."
 
-az acr login -n renovatetalkacr
-docker pull pietervincken/renovate-talk-java-demo-app:base
-docker tag pietervincken/renovate-talk-java-demo-app:base renovatetalkacr.azurecr.io/renovate-talk-java-demo-app:1.4.4
-docker push renovatetalkacr.azurecr.io/renovate-talk-java-demo-app:1.4.4
+temp=$(mktemp -d)
+
+cd $temp
+
+git clone https://github.com/pietervincken/renovate-talk-java-demo-app.git
+cd renovate-talk-java-demo-app
+latest_tag=$(git tag | sort -V | tail -1 | sed 's|v||') # remove v from tag name!
+echo "Using tag $latest_tag"
+
+az acr import --name renovatetalkacr --source docker.io/pietervincken/renovate-talk-java-demo-app:base --image renovate-talk-java-demo-app:$latest_tag
