4870402bad8b4175e2fb134e4cd95fcb1fca37a3
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-10-03 13:40:27 +0200

    fix: demo setup

--- 4-upload-app-image.sh
+++ 4-upload-app-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e;
 
 temp=$(mktemp -d)
 
@@ -10,4 +10,7 @@ cd renovate-talk-java-demo-app
 latest_tag=$(git tag | sort -V | tail -1 | sed 's|v||') # remove v from tag name!
 echo "Using tag $latest_tag"
 
-az acr import --name renovatetalkacr --source docker.io/pietervincken/renovate-talk-java-demo-app:base --image renovate-talk-java-demo-app:$latest_tag
+docker build --build-arg=APP_VERSION=$latest_tag -t renovatetalkacr.azurecr.io/renovate-talk-java-demo-app:$latest_tag .
+az acr login -n renovatetalkacr
+
+docker push renovatetalkacr.azurecr.io/renovate-talk-java-demo-app:$latest_tag
