db2192bad8d85291ca2a0b6d3da96bcd9e17feb8
pietervincken <4153479+pietervincken@users.noreply.github.com>
2023-01-13 10:28:43 +0100

    refactor: remove azure specific files (WIP)
    feat: added service account to renovate

--- 1-terraform-apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd terraform
-terraform init -backend-config=config.azurerm.tfbackend
-terraform apply
-terraform output -json > output.json
-cd ..
--- 99-terraform-destroy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd terraform
-terraform init -backend-config=config.azurerm.tfbackend
-terraform destroy
-cd ..
