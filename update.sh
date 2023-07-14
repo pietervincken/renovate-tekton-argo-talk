e373caae47608c10e326cdec567f21bcc7c8efea
pietervincken <4153479+pietervincken@users.noreply.github.com>
2023-07-14 14:30:13 +0200

    re-add traefik

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ get_latest_release() {
 # helm repo add traefik https://helm.traefik.io/traefik
 # helm repo add external-secrets https://charts.external-secrets.io
 # helm repo add bitnami https://charts.bitnami.com/bitnami
-# helm repo update
+helm repo update
 
 # argoCDVersion=$(get_latest_release "argoproj/argo-cd")
 # cd k8s/argocd
@@ -48,21 +48,21 @@ get_latest_release() {
 # cd ../../../..
 # echo "Upgraded certmanager to $certManagerVersion"
 
-# cd k8s/traefik
-# rm -rf resources/render/
-# mkdir -p resources/render
-# helm template traefik traefik/traefik \
-#   -n traefik \
-#   --set globalArguments= \
-#   --set providers.kubernetesIngress.publishedService.enabled=true \
-#   --set ingressRoute.dashboard.enabled=false \
-#   | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
-# curl -sL https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml  | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
-# curl -sL https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
-# cd resources/render/
-# kustomize create app --recursive --autodetect
-# cd ../../../..
-# echo "Upgraded traefik"
+cd k8s/traefik
+rm -rf resources/render/
+mkdir -p resources/render
+helm template traefik traefik/traefik \
+  -n traefik \
+  --set globalArguments= \
+  --set providers.kubernetesIngress.publishedService.enabled=true \
+  --set ingressRoute.dashboard.enabled=false \
+  | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
+curl -sL https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml  | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
+curl -sL https://raw.githubusercontent.com/traefik/traefik/master/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml | yq -s '"resources/render/" + .metadata.name + "-" + .kind + ".yml"' -
+cd resources/render/
+kustomize create app --recursive --autodetect
+cd ../../../..
+echo "Upgraded traefik"
 
 # mkdir -p k8s/prometheus-operator || true
 # cd k8s/prometheus-operator
